7ca7151ca205adbf555b4de9747a5087a5f1a549
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2016-07-15 18:42:42 -0300

    Change to gradle

--- src/main/java/ml/jmoodle/config/MoodleConfig.java
+++ src/main/java/ml/jmoodle/config/MoodleConfig.java
@@ -4,10 +4,21 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MoodleConfig {
+	public static final String MOODLE_REST_ENDPOINT = "/webservice/rest/server.php";
+
 	private URL moodleURL;
 
-	public MoodleConfig(URL moodleURL) {
-		this.moodleURL = moodleURL;
+	/**
+	 * 
+	 * Builds Moodle configuration
+	 * 
+	 * @param moodleURL
+	 *            must be the same value of $CFG->wwwroot in moodle config.php
+	 *            file
+	 * @throws MalformedURLException
+	 */
+	public MoodleConfig(URL moodleURL) throws MalformedURLException {
+		this(moodleURL.toString());
 	}
 
 	public MoodleConfig(String moodleURL) throws MalformedURLException {
@@ -15,13 +26,14 @@ public class MoodleConfig {
 				|| moodleURL.trim().toLowerCase().startsWith("https://"))) {
 			moodleURL = "http://" + moodleURL;
 		}
-		this.moodleURL = new URL(moodleURL);
+		this.moodleURL = new URL(moodleURL + MOODLE_REST_ENDPOINT);
 	}
 
 	/**
 	 * @return the moodleURL
+	 * 
 	 */
 	protected URL getMoodleURL() {
-		return moodleURL;
+		return this.moodleURL;
 	}
 }
--- src/test/java/ml/jmoodle/config/MoodleConfigTest.java
+++ src/test/java/ml/jmoodle/config/MoodleConfigTest.java
@@ -1,22 +1,28 @@
 package ml.jmoodle.config;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutorCompletionService;
+
 import static org.mockito.Mockito.*;
 import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-
-
+import static org.junit.Assert.*;
 
 public class MoodleConfigTest {
-	
-	@Rule
-    public MockitoRule rule = MockitoJUnit.rule();
 
-	
+	@Rule
+	public MockitoRule rule = MockitoJUnit.rule();
+
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+
 	public static void setUpBeforeClass() throws Exception {
-		//MockitoAnnotations.initMocks(MoodleConfigTest.class);
+		// MockitoAnnotations.initMocks(MoodleConfigTest.class);
 	}
 
 	public static void tearDownAfterClass() throws Exception {
@@ -28,12 +34,26 @@ public class MoodleConfigTest {
 	public void tearDown() throws Exception {
 	}
 
-	public final void MoodleConfigConstructorTest() {
+	@Test
+	public final void MoodleConfigConstructorTest() throws MalformedURLException {
 		URL testUrl = new URL("http://test.com");
-		MoodleConfig config=spy(new MoodleConfig(testUrl));
-		doReturn(testUrl).when(config).getMoodleURL();
+		MoodleConfig config=new MoodleConfig(testUrl);
+		assertEquals(testUrl.toString()+"/webservice/rest/server.php", config.getMoodleURL().toString());
+		
+		
+		MoodleConfig config2=new MoodleConfig("http://test.com");
+		assertEquals(testUrl.toString()+"/webservice/rest/server.php", config2.getMoodleURL().toString());
+		
+		MoodleConfig config3=new MoodleConfig("test.com");
+		assertEquals(testUrl.toString()+"/webservice/rest/server.php", config3.getMoodleURL().toString());
+
+
+//	    Will never throw MalformedURLException because the proctocol is always secified.
+//		exception.expect(java.net.MalformedURLException.class);
+//		new URL("http");
+////		new MoodleConfig("<invaid-           %Wccc fez URL>");
+		
 		
 	}
 
-	
 }
