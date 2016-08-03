b76b5111322d09649a4897c384ae44f4d05b8c3f
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2016-08-03 13:00:10 -0300

    Add Create user exception

---
+++ src/main/java/ml/jmoodle/functions/exceptions/MoodleRestCreateUserException.java
@@ -0,0 +1,26 @@
+package ml.jmoodle.functions.exceptions;
+
+public class MoodleRestCreateUserException extends MoodleWSFucntionException {
+
+	private static final long serialVersionUID = 4959575765193834380L;
+
+	public MoodleRestCreateUserException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MoodleRestCreateUserException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MoodleRestCreateUserException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MoodleRestCreateUserException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
